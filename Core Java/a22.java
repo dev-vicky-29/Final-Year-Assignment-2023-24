e819dfea2c85539bb8641e3d3aec8099f20d41f2
cs-vicky-29 <vickydeshmukh2509@gmail.com>
2023-12-10 23:25:49 +0530

    Assignment 2.2 Added
    Write a Program to illustrate Explicit Type Casting

---
+++ Core Java/a22.java
@@ -0,0 +1,13 @@
+//Write a Program to illustrate Explicit Type Casting
+public class a22
+{
+   public static void main(String[] args)
+    {
+       double d = 200.06;
+       long l = (long)d;
+       int i = (int)l;
+       System.out.println("Double Data type value "+d);
+       System.out.println("Long Data type value "+l);
+       System.out.println("Int Data type value "+i);
+    }
+}
