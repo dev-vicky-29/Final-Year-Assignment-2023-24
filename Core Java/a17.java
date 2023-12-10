3cffc31504a22ec8a852e1e042cccfa780e7d4e6
cs-vicky-29 <vickydeshmukh2509@gmail.com>
2023-12-10 23:20:25 +0530

    Assignment 1.7 Added
    Write a Program to Check an Armstrong Number

---
+++ Core Java/a17.java
@@ -0,0 +1,23 @@
+//Write a Program to Check an Armstrong Number
+public class a17
+{
+    public static void main(String[] args)
+     {
+        int number = 9474, originalNumber, remainder, result = 0, n = 0;
+        originalNumber = number;
+        for (;originalNumber != 0; originalNumber /= 10)
+        {
+            n++;
+        }
+        originalNumber = number;
+        for (;originalNumber != 0; originalNumber /= 10)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, n);
+        }
+        if(result == number)
+            System.out.println(number + " is an Armstrong number");
+else
+            System.out.println(number + " is not an Armstrong number.");
+    }
+}
