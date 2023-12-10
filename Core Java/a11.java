cee67690484fb6af62b42ee4d9cec6906b934769
cs-vicky-29 <vickydeshmukh2509@gmail.com>
2023-12-10 22:42:26 +0530

    Update a11.java

--- Core Java/a11.java
+++ Core Java/a11.java
@@ -1,15 +1,17 @@
+//Write a Program to Perform basic Calculator Operations
 import java.util.*;
-public class Calculator
+//public class Calculator
+public class a11
 {
   public static void main(String[] args)
   {
-   Scanner scn=new Scanner(System.in);
+   Scanner sc=new Scanner(System.in);
    System.out.println("Enter First Number:");
-   int num1=scn.nextInt();
+   int num1=sc.nextInt();
    System.out.println("Enter second Number:");
-  int num2=scn.nextInt();
+  int num2=sc.nextInt();
   System.out.println("Enter Operator:");
-  char operator=scn.next().charAt(0);
+  char operator=sc.next().charAt(0);
   int  result;
   switch(operator)
  {
