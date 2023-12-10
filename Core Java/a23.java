c55cd21265a51e806d4ab934881e01541958ce2f
cs-vicky-29 <vickydeshmukh2509@gmail.com>
2023-12-10 23:28:01 +0530

    Assignment 2.3 Added
    Write a Program to Convert String into int

---
+++ Core Java/a23.java
@@ -0,0 +1,11 @@
+//Write a Program to Convert String into int
+public class a23 
+{
+   public static void main(String[] args) 
+    {
+       String data = "10"; 
+       System.out.println("The string value is: " + data);
+       int num = Integer.parseInt(data);
+       System.out.println("The integer value is: " + num);
+     }
+}
