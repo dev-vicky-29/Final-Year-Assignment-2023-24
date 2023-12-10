1d10bf89eed2d2a186e40e144a45dd5c9131dd49
cs-vicky-29 <vickydeshmukh2509@gmail.com>
2023-12-10 23:22:43 +0530

    Assignment 2.1
    Write a Program to illustrate Implicit Type Casting

---
+++ Core Java/a21.java
@@ -0,0 +1,14 @@
+//Write a Program to illustrate Implicit Type Casting
+public class a21 
+{
+   public static void main(String[] args)
+    {
+        int i = 200;
+        long l = i;
+        float f = l;
+        System.out.println("Int value "+i);
+        System.out.println("Long value "+l);
+        System.out.println("Float value "+f);
+     }
+}
+	
